ec53670d96fb189132d6049fb5c610f28e8a9394
PironSteve <steve.piron@gmail.com>
2018-11-06 10:27:46 +0100

    commit serie 1 et 2

---
+++ 02-maths/02-calculator-two/script.js
@@ -0,0 +1,40 @@
+/* becode/javascript
+ *
+ * /02-maths/02-calculator-two/script.js - 2.2: calculatrice (2)
+ *
+ * coded by leny@BeCode
+ * started at 26/10/2018
+ */
+
+// NOTE: don't focus on the existing code structure for now.
+// You will have time to focus on it later.
+
+(function () {
+    // to get the value of an input: document.getElememntById("element-id").value
+
+    var performOperation = function (operation) {
+
+        switch (operation) {
+            case "addition":
+                alert(parseInt(document.getElementById("op-one").value) + parseInt(document.getElementById("op-two").value));
+                break;
+            case "substraction":
+                alert(parseInt(document.getElementById("op-one").value) - parseInt(document.getElementById("op-two").value));
+                break;
+            case "multiplication":
+                alert(parseInt(document.getElementById("op-one").value) * parseInt(document.getElementById("op-two").value));
+                break;
+            case "division":
+                alert(parseInt(document.getElementById("op-one").value) / parseInt(document.getElementById("op-two").value));
+                break;
+            default:
+                break;
+        }
+    };
+
+    Array.from(document.querySelectorAll("button.operator")).forEach(function ($btn) {
+        $btn.addEventListener("click", function () {
+            performOperation($btn.id);
+        });
+    });
+})();
