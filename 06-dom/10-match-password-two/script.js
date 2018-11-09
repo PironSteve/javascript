271d6cd8563198643f31440bdd6950eb02ee9b1e
PironSteve <steve.piron@gmail.com>
2018-11-09 15:49:00 +0100

    serie 6 ex 10

--- 06-dom/10-match-password-two/script.js
+++ 06-dom/10-match-password-two/script.js
@@ -9,8 +9,24 @@
 // NOTE: don't focus on the existing code structure for now.
 // You will have time to focus on it later.
 
-(function() {
+(function () {
 
-    // your code here
+    document.getElementById("run").addEventListener('click', function () {
+
+        var password = document.getElementsByTagName("input")[0];
+        var confirm = document.getElementsByTagName("input")[1];
+        var att = document.createAttribute("class");
+        var att2 = document.createAttribute("class");
+        att.value = "error";
+        att2.value = "error";
+
+        if (password.value !== confirm.value) {
+            password.setAttributeNode(att);
+            confirm.setAttributeNode(att2);
+        }
+
+    })
+
+
 
 })();
