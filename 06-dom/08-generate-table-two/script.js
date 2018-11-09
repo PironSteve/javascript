b835e5d62abade4e0beee722655fff93b8bfe8aa
PironSteve <steve.piron@gmail.com>
2018-11-09 14:20:47 +0100

    serie 6 ex 8

--- 06-dom/08-generate-table-two/script.js
+++ 06-dom/08-generate-table-two/script.js
@@ -9,8 +9,24 @@
 // NOTE: don't focus on the existing code structure for now.
 // You will have time to focus on it later.
 
-(function() {
+(function () {
 
-    // your code here
+    var tab = document.createElement("table");
+    var tabBody = document.createElement("tbody");
 
+    for (var i = 1; i <= 10; i++) {
+        var row = document.createElement("tr");
+
+        for (var j = 1; j <= 10; j++) {
+            var cell = document.createElement("td");
+            var cellText = document.createTextNode(i * j);
+            cell.appendChild(cellText);
+            row.appendChild(cell);
+        }
+        tabBody.appendChild(row);
+    }
+    tab.appendChild(tabBody);
+    tab.setAttribute("border", "2");
+
+    document.getElementById("target").appendChild(tab);
 })();
