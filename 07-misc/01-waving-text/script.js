ae0c08478d6b06598929071e2b88d143d5137c77
PironSteve <steve.piron@gmail.com>
2018-11-13 12:15:03 +0100

    serie 7 ex 1

--- 07-misc/01-waving-text/script.js
+++ 07-misc/01-waving-text/script.js
@@ -11,6 +11,17 @@
 
 (function() {
 
-    // your code here
+    var size = ["10px","15px","20px","25px","30px","25px","20px","15px","10px"];
+    var target = document.getElementById("target")
+    var text = target.innerHTML;
+    target.innerHTML = "";
+    var tabText = text.split("");
+    
+    for(var i=0; i<tabText.length;i++){
+        var span = document.createElement("span");
+        span.innerText = tabText[i];
+        span.style.fontSize = size[i%size.length];
+        target.appendChild(span);
+    }
 
 })();
