a0953cb67bccfd10f29f7dfb60a2bfc691343f9c
PironSteve <steve.piron@gmail.com>
2018-11-16 10:14:15 +0100

    serie 4 ex 3 ES6

--- 04-dates/03-age-by-select/script.js
+++ 04-dates/03-age-by-select/script.js
@@ -15,23 +15,20 @@
 
 
 
-    document.getElementById("run").addEventListener("click", function () {
-        var now = new Date();
-        var year = document.getElementById("dob-year").value;
-        var month = document.getElementById("dob-month").value;
-        var day = document.getElementById("dob-day").value;
-        var anniversary = new Date(year + "/" + month + "/" + day);
-        var age = now.getFullYear() - anniversary.getFullYear();
+    document.getElementById("run").addEventListener("click", ()=> {
+        const now = new Date();
+        let year = document.getElementById("dob-year").value;
+        let month = document.getElementById("dob-month").value;
+        let day = document.getElementById("dob-day").value;
+        const anniversary = new Date(`${year}/${month}/${day}`);
+        let age = now.getFullYear() - anniversary.getFullYear();
 
         anniversary.setFullYear(now.getFullYear());
 
         if (now < anniversary) {
             age--;
         }
-
-        alert("Tu as " + age + " ans");
-
-
+        alert(`Tu as ${age} ans`);
     })
 
 })();
